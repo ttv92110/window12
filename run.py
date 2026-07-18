df980bbd7b5ade82b31257743625ad9a57e4c2f9
Arshad Ali <arsk92110@gmail.com>
2026-07-18 16:39:34 +0500

    changes newly

--- run.py
+++ run.py
@@ -4,7 +4,7 @@ if __name__ == "__main__":
     uvicorn.run(
         "api.main:app", 
         host="127.0.0.1", 
-        port=8018, 
+        port=8020, 
         reload=True,
         reload_dirs=["api", "templates", "static"]
     )
